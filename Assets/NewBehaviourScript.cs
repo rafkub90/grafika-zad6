2bed0250267d7e0d449a7c01817ebc8b93dcea8a
DESKTOP-HUMKCOQ\rafku <rafkub90@gmail.com>
2018-04-22 21:34:02 +0200

    no message

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private Animator anim = null;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("d"))
+        {
+            anim.SetBool("keyRight1", false);
+            anim.SetBool("keyRight" , true);
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            anim.SetBool("keyRight", false);
+            anim.SetBool("keyRight1", true);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            anim.SetBool("keyLeft1", false);
+            anim.SetBool("keyLeft", true);
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            anim.SetBool("keyLeft", false);
+            anim.SetBool("keyLeft1", true);
+        }
+    }
+
+
+}
